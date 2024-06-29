1e6c138f968f3da5b724dd45ad87469e6dff9fd0
Shrey0207 <shreytolasaria4297@gmail.com>
2024-06-30 01:41:07 +0530

    implemented get current user api

--- src/pages/private/home/index.tsx
+++ src/pages/private/home/index.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getCurrentUser } from "../../../api-services/user-service";
+import { message } from "antd";
 
 function Homepage() {
-  return <div>Homepage</div>;
+  const [user, setUser] = useState<any>(null);
+
+  const getData = async () => {
+    try {
+      const response = await getCurrentUser();
+      setUser(response.data);
+    } catch (error: any) {
+      message.error(error.message);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className="p-5">
+      <h1>Homepage</h1>
+      <p>Welcome,{user?.name}!</p>
+    </div>
+  );
 }
 
 export default Homepage;
