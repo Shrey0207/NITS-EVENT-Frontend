4fef2cb98a62d25961512c80322a71fd14a16779
Shrey0207 <shreytolasaria4297@gmail.com>
2024-06-29 21:08:50 +0530

    implemented login page frontend

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,4 +1,50 @@
+import { Button, Form, Input } from "antd";
+import WelcomeContent from "../common/welcome-content";
+import { Link } from "react-router-dom";
+
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const onFinish = (values: never) => {
+    console.log("Received values:", values);
+  };
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div className="col-span-1 lg:flex hidden">
+        <WelcomeContent />
+      </div>
+      <div className="h-screen flex items-center justify-center">
+        <Form
+          className="flex flex-col gap-5 w-96"
+          layout="vertical"
+          onFinish={onFinish}
+        >
+          <h1 className="text-2xl font-bold text-gray-600">
+            Login your account
+          </h1>
+          <Form.Item
+            name="email"
+            required
+            label="Email"
+            rules={[{ required: true }]}
+          >
+            <Input placeholder="Email" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            required
+            label="Password"
+            rules={[{ required: true }]}
+          >
+            <Input.Password placeholder="Password" />
+          </Form.Item>
+
+          <Button type="primary" htmlType="submit" block>
+            Login
+          </Button>
+          <Link to="/register">Don't have an account ? Register</Link>
+        </Form>
+      </div>
+    </div>
+  );
 }
 export default LoginPage;
